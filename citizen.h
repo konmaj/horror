8e11873d5a2629f150f1b9a8fc32b43452851fb7
Marcin Wawerka <marcin.wawerka@gmail.com>
2016-11-25 20:58:25 +0100

    Initial version, not complete

--- citizen.h
+++ citizen.h
@@ -1,5 +1,45 @@
 // Konrad Majewski, Marcin Wawerka
 #ifndef CITIZEN_H_
 #define CITIZEN_H_
+#include <string>
+#include <iostream>
+#include <cassert>
+
+template <typename T, T minAge, T maxAge,
+	typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+class Citizen{
+	private:
+		T health;
+		T age;
+		
+		T min (T a, T b) {
+			return a < b ? a : b;
+		}
+		
+	public:
+		T getHealth() {
+			return health;
+		}
+		
+		T getAge() {
+			return age;
+		}
+		
+		Citizen (T health, T age) {
+			assert(age >= minAge && age <= maxAge);
+			this->health = health;
+			this->age = age;
+		}
+		
+		void takeDamage(T damage) {
+			this->health -= min(damage, this->health);
+		}
+}; 
+
+template<typename T>
+using Teenager = Citizen<T, 11, 18>;
+
+template<typename T>
+using Adult = Citizen<T, 19, 100>;
 
 #endif //CITIZEN_H_
--- monster.h
+++ monster.h
@@ -2,4 +2,59 @@
 #ifndef MONSTER_H_
 #define MONSTER_H_
 
+#include <typeinfo>
+#include "citizen.h"
+
+enum monst {
+	mummy = 1,
+	vampire = 2,
+	zombie = 3
+};
+
+template <typename T, enum monst,
+	typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+class Monster{
+	private:
+		T health;
+		T attackPower;
+		
+		T min (T a, T b) {
+			return a < b ? a : b;
+		}
+		
+	public:
+		
+		const std::type_info& valueType = typeid(T);
+		
+		T getHealth() {
+			return health;
+		}
+		
+		T getAttackPower() {
+			return attackPower;
+		}
+		
+		Monster (T health, T attackPower) {
+			this->health = health;
+			this->attackPower = attackPower;
+		}
+		
+		void takeDamage(T damage) {
+			health -= min(damage, health);
+		}
+}; 
+
+template<typename T>
+using Mummy = Monster<T, mummy>;
+
+template<typename T>
+using Zombie = Monster<T, zombie>;
+
+template<typename T>
+using Vampire = Monster<T, vampire>;
+
+template<typename M, typename U>
+void attack(M monster, U victim) {
+	victim->takeDamage(monster->getAttackPower());
+};
 #endif //MONSTER_H_
