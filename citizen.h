3ebe9b3aae8b2e30cdd168514eb25955ca4de1f8
Marcin Wawerka <marcin.wawerka@gmail.com>
2016-11-26 18:03:37 +0100

    Add sheriff through enable_ifs

--- citizen.h
+++ citizen.h
@@ -1,16 +1,16 @@
 // Konrad Majewski, Marcin Wawerka
 #ifndef CITIZEN_H_
 #define CITIZEN_H_
-#include <string>
-#include <iostream>
 #include <cassert>
+#include <type_traits>
 
-template <typename T, T minAge, T maxAge,
+template <typename T, T minAge, T maxAge, bool isArmed,
 	typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
 class Citizen{
 	private:
 		T health;
 		T age;
+		T attackPower = 0;
 		
 		T min (T a, T b) {
 			return a < b ? a : b;
@@ -25,10 +25,24 @@ class Citizen{
 			return age;
 		}
 		
+		template<typename Q = T, typename = typename std::enable_if<!isArmed, Q>::type>
 		Citizen (T health, T age) {
 			assert(age >= minAge && age <= maxAge);
 			this->health = health;
 			this->age = age;
+		}
+		
+		template<typename Q = T, typename = typename std::enable_if<isArmed, Q>::type>
+		Citizen (T health, T age, T attackPower) {
+			assert(age >= minAge && age <= maxAge);
+			this->health = health;
+			this->age = age;
+			this->attackPower = attackPower;
+		}
+		
+		template<typename Q = T, typename = typename std::enable_if<isArmed, Q>::type>
+		T getAttackPower() {
+			return attackPower;
 		}
 		
 		void takeDamage(T damage) {
@@ -37,9 +51,12 @@ class Citizen{
 }; 
 
 template<typename T>
-using Teenager = Citizen<T, 11, 18>;
+using Teenager = Citizen<T, 11, 18, false>;
 
 template<typename T>
-using Adult = Citizen<T, 19, 100>;
+using Adult = Citizen<T, 19, 100, false>;
+
+template<typename T>
+using Sheriff = Citizen<T, 19, 100, true>;
 
 #endif //CITIZEN_H_
