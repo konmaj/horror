81dfe93e4460c485d371998f8592e1454d7475a3
Konrad Majewski <km371194@students.mimuw.edu.pl>
2016-12-01 16:13:54 +0100

    Fibonacci numbers

--- smalltown.h
+++ smalltown.h
@@ -5,12 +5,12 @@
 #include "citizen.h"
 #include "monster.h"
 
+#include <algorithm>
+#include <array>
 #include <iostream>
 #include <string>
 #include <tuple>
 #include <type_traits>
-
-// TODO Fibonacci numbers class
 
 template<typename M, typename U, U t0, U t1, typename... C>
 class SmallTown {
@@ -19,9 +19,44 @@ class SmallTown {
 		U time_;
 		std::tuple<C...> citizens_;
 		size_t aliveCounter;
+		
+		static constexpr U fibonacci(size_t n) {
+			U a = 1, b = 1;
+			for (size_t i = 0; i < n; i++) {
+				U c = a + b;
+				a = b;
+				b = c;
+			}
+			return a;
+		}
+		
+		static constexpr size_t calcLimit() {
+			size_t n = 0;
+			while (fibonacci(n) <= t1) {
+				n++;
+			}
+			return n;
+		}
+		
+		template<size_t N, U... FibNums>
+		static constexpr typename 
+		std::enable_if_t<(N == sizeof...(FibNums)), std::array<U, N>> makeArray() {
+			return std::array<U, N>{{FibNums...}};
+		}
+
+		template <size_t N, U... FibNums>
+		static constexpr typename 
+		std::enable_if_t<(N != sizeof...(FibNums)), std::array<U, N>> makeArray() {
+			return makeArray<N, FibNums..., fibonacci(sizeof...(FibNums))>();
+		}
+		
+		static const std::array<U, calcLimit()>& fibonacciNumbers() {
+			static const std::array<U, calcLimit()> fibArray = makeArray<calcLimit()>();
+			return fibArray;
+		}
 
 		template<size_t i = 0>
-		typename std::enable_if_t<i < sizeof...(C), int> countAliveCitizens() {
+		typename std::enable_if_t<(i < sizeof...(C)), int> countAliveCitizens() {
 			if (std::get<i>(citizens_).getHealth() != 0) {
 				return 1 + countAliveCitizens<i + 1>();
 			}
@@ -31,25 +66,24 @@ class SmallTown {
 		}
 
 		template<size_t i = 0>
-		typename std::enable_if_t<i == sizeof...(C), int> countAliveCitizens() {
+		typename std::enable_if_t<(i == sizeof...(C)), int> countAliveCitizens() {
 			return 0;
 		}
 
 
 		template<size_t i = 0>
-		typename std::enable_if_t<i < sizeof...(C), void> attackCitizens() {
+		typename std::enable_if_t<(i < sizeof...(C)), void> attackCitizens() {
 			attack(monster_, std::get<i>(citizens_));
 			attackCitizens<i + 1>();
 		}
 
 		template<size_t i = 0>
-		typename std::enable_if_t<i == sizeof...(C), void> attackCitizens() {}
+		typename std::enable_if_t<(i == sizeof...(C)), void> attackCitizens() {}
 
   	public:
-		SmallTown(const M &monster, const C&... citizens)
-				: monster_(monster), time_(t0), citizens_(citizens...) {
+		SmallTown(const M &monster, const C&... citizens) 
+		: monster_(monster), time_(t0), citizens_(citizens...) {
 			static_assert(0 <= t0 && t0 <= t1, "INCORRECT TIME VALUES");
-
 			aliveCounter = countAliveCitizens();
 		}
 
@@ -69,11 +103,11 @@ class SmallTown {
 			else if (aliveCounter == 0) {
 				std::cout << "MONSTER WON\n";
 			}
-			else if (time_ == 0) {	// TODO: Fibonacci numbers
+			else if (fibonacciNumbers().size() > 0 && std::binary_search(fibonacciNumbers().begin(), fibonacciNumbers().end(), time_)) {
 				attackCitizens();
 				aliveCounter = countAliveCitizens();
 			}
-			time_ = (time_ + timeStep) % t1;
+			time_ = (time_ + timeStep) % (t1 + 1);
 		}
 };
 
